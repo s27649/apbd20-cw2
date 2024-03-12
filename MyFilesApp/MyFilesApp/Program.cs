457632b0687a5a36fe59957f2a8fec6d10b0b56e
s27649 <s27649@pjwstk.edu.pl>
2024-03-12 17:00:13 +0100

    Add GetAverage metod

--- MyFilesApp/MyFilesApp/Program.cs
+++ MyFilesApp/MyFilesApp/Program.cs
@@ -1,2 +1,11 @@
-﻿
-Console.WriteLine("Hello, hohey");
+﻿static double GetAverage(int[] numbers)
+{
+    int suma = 0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        suma += numbers[i];
+        
+    }
+
+    return (double)suma / numbers.Length;
+}
