343f4c06eea348e3e4acd0e1d5f9878961814d84
Настя Лущицкая <s27649@pjwstk.edu.pl>
2024-03-14 16:31:06 +0100

    Dodana branch feature-max

--- MyFilesApp/MyFilesApp/Program.cs
+++ MyFilesApp/MyFilesApp/Program.cs
@@ -13,3 +13,21 @@
 int[] numbers={1,2,3,4};
 double average = GetAverage(numbers);
 Console.WriteLine(average);
+
+
+static int GetMax(int[] numbers)
+{
+    int max = 0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i]> max)
+        {
+           max=numbers[i];
+        }
+    }
+    return max;
+}
+
+int[] numbers1={5,7,11,0};
+int max = GetMax(numbers1);
+Console.WriteLine(max);
